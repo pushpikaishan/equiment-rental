201c14933bab26bdce8a7e346ab4782839d45de2
Pushpika Ishan <pushpikaishan@gmail.com>
2025-10-11 13:41:21 +0530

    ADD DISTRIK
    CJ

--- mern/frontend/src/component/userregister/supplierRegister.js
+++ mern/frontend/src/component/userregister/supplierRegister.js
@@ -705,9 +705,31 @@ function SupplierRegister() {
                   }}
                 >
                   <option value="">Select your district</option>
-                  <option value="galle">Galle</option>
-                  <option value="matara">Matara</option>
-                  <option value="hambantota">Hambantota</option>
+                  <option value="Ampara">Ampara</option>
+                  <option value="Anuradhapura">Anuradhapura</option>
+                  <option value="Badulla">Badulla</option>
+                  <option value="Batticaloa">Batticaloa</option>
+                  <option value="Colombo">Colombo</option>
+                  <option value="Gampaha">Gampaha</option>
+                  <option value="Galle">Galle</option>
+                  <option value="Hambantota">Hambantota</option>
+                  <option value="Jaffna">Jaffna</option>
+                  <option value="Kalutara">Kalutara</option>
+                  <option value="Kandy">Kandy</option>
+                  <option value="Kegalle">Kegalle</option>
+                  <option value="Kilinochchi">Kilinochchi</option>
+                  <option value="Kurunegala">Kurunegala</option>
+                  <option value="Matale">Matale</option>
+                  <option value="Matara">Matara</option>
+                  <option value="Mannar">Mannar</option>
+                  <option value="Monaragala">Monaragala</option>
+                  <option value="Mullaitivu">Mullaitivu</option>
+                  <option value="Muwara-eliya">Nuwara Eliya</option>
+                  <option value="Polonnaruwa">Polonnaruwa</option>
+                  <option value="Puttalam">Puttalam</option>
+                  <option value="Ratnapura">Ratnapura</option>
+                  <option value="Trincomalee">Trincomalee</option>
+                  <option value="Vavuniya">Vavuniya</option>
                 </select>
               </div>
               {errors.district && <div style={errorMessageStyle}>{errors.district}</div>}
